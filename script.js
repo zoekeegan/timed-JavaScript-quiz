b450b13dfd4e43442528a3dd6478913ba81e39ea
Zoe Taron <zoearan16@gmail.com>
2023-10-15 12:09:10 -0400

    new html

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const startButton = document.getElementById('start')
+// const startButton = document.getElementById('start');
 
-startButton.addEventListener('click', startQuiz)
+// startButton.addEventListener('click', startQuiz);
 
-function startQuiz () {
-    console.log('Started')
-}
+// function startQuiz () {
+//     console.log('Started')
+//     // startButton.classList.add('hide')
+// }
+
+// const timer = document.getElementById('timer')
+
+// timer.addEventListener('click', startTimer)
+
+// function startTimer () {
+//     console.log('timer started')
+// }
+
