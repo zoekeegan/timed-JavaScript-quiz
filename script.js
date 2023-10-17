399f19e5aaea5991d798ff8af5644991972573c3
Zoe Taron <zoearan16@gmail.com>
2023-10-17 11:14:49 -0400

    added js, first page done

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-// const startButton = document.getElementById('start');
+document.addEventListener('DOMContentLoaded', function () {
+    const startButton = document.getElementById('start-button');
+    const timerDisplay = document.getElementById('timer');
+    const pageOne = document.getElementById ('intro');
+    const questionOne = document.getElementById ('question1');
+  
+    let countdown; 
 
-// startButton.addEventListener('click', startQuiz);
+    questionOne.style.display = 'none';
+  
+    startButton.addEventListener('click', startQuiz);
+  
+    function startQuiz() {
+      startButton.style.display = 'none';
+      pageOne.style.display = 'none';
+      timerDisplay.style.display = 'block';
+      questionOne.style.display = ''
+      // console.log ("started")
+  
+      let timeLeft = 75;
+      timerDisplay.textContent = timeLeft;
+  
+      countdown = setInterval(function () {
+        timeLeft--;
+        timerDisplay.textContent = timeLeft;
+        if (timeLeft <= 0) {
+          clearInterval(countdown);
+        }
+      }, 1000);
+    }
+  });
 
-// function startQuiz () {
-//     console.log('Started')
-//     // startButton.classList.add('hide')
-// }
 
-// const timer = document.getElementById('timer')
 
-// timer.addEventListener('click', startTimer)
 
-// function startTimer () {
-//     console.log('timer started')
-// }
+
+
 
